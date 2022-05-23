65e73245cca90c548edfb6f49ce617b42f7ad7c8
Chris Queen <chrisqueen10@gmail.com>
2022-05-22 21:05:59 -0400

    Added 3d terrain and blue sky to arizona map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,6 +1,6 @@
 // If you don't want to use TypeScript you can delete this file!
 import * as React from "react"
-import { PageProps, Link, graphql } from "gatsby"
+import { PageProps } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -9,14 +9,45 @@ import mapboxgl from "mapbox-gl"
 
 const MapPage: React.FC<PageProps> = () => {
   React.useEffect(() => {
-    mapboxgl.accessToken = process.env.GATSBY_MAP_TOKEN
-    const map = new mapboxgl.Map({
-      container: "map", // container ID
-      style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [-112.16145, 33.084983333333334],
-      zoom: 9,
-    })
-    map.resize()
+    const initMap = async () => {
+      mapboxgl.accessToken = process.env.GATSBY_MAP_TOKEN
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/satellite-v9", // style URL
+        center: [-112.16145, 33.084983333333334],
+        zoom: 16,
+        pitch: 0,
+      })
+      map.resize()
+
+      await map.once("load")
+
+      // Add some 3d terrain
+      map.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.terrain-rgb",
+        tileSize: 512,
+        maxzoom: 14,
+      })
+      
+      map.setTerrain({
+        source: "mapbox-dem",
+        exaggeration: 1.5,
+      })
+
+      // sky
+      map.addLayer({
+        id: 'sky',
+        type: 'sky',
+        paint: {
+          'sky-type': 'atmosphere',
+          'sky-atmosphere-sun': [0.0, 0.0],
+          'sky-atmosphere-sun-intensity': 15,
+        },
+      })
+    }
+
+    initMap()
   }, [])
 
   return (
